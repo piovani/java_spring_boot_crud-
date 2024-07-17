65f02beb3f5dc8580888473d63a37acd6209ab9c
Allison Piovani <apiovani25@gmail.com>
2024-07-16 21:59:45 -0300

    first commit

---
+++ src/main/java/com/example/java_spring_boot_crud/JavaSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_spring_boot_crud/JavaSpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_boot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
