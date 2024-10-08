d9f9d2f9d1beb337a8d3063ce0aff9f2cf688fce
Allison Piovani <apiovani25@gmail.com>
2024-10-08 18:54:16 -0300

    fixed: bug time

--- src/main/java/com/example/java_spring_boot_crud/entrypoint/api/controller/ProductController.java
+++ src/main/java/com/example/java_spring_boot_crud/entrypoint/api/controller/ProductController.java
@@ -3,13 +3,20 @@ package com.example.java_spring_boot_crud.entrypoint.api.controller;
 import com.example.java_spring_boot_crud.core.common.dto.ProductRecordDto;
 import com.example.java_spring_boot_crud.dataprovider.database.entity.ProductEntity;
 import com.example.java_spring_boot_crud.dataprovider.database.repositores.ProductRepository;
-import com.example.java_spring_boot_crud.entrypoint.api.expection.CreateProductExepection;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -32,13 +39,7 @@ public class ProductController {
         var product = new ProductEntity();
         BeanUtils.copyProperties(productRecordDto, product);
 
-        var newProduct = productRepository.save(product);
-
-        if (true) {
-            throw new CreateProductExepection();
-        }
-
-        return ResponseEntity.status(HttpStatus.CREATED).body(newProduct);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(product));
     }
 
     @GetMapping("/{id}")
